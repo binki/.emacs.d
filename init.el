bfa6d75ae036e7ba0cea47e000ba3a7715026df6
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2017-06-24 15:08:59 -0400

    Fix default frame colors to be green on black.

--- init.el
+++ init.el
@@ -9,6 +9,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default-frame-alist
+    (quote
+     ((background-color . "black")
+      (foreground-color . "green"))))
  '(package-archives
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
