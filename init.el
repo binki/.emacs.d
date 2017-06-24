f2e832bb9da1b1755f23e02813093868cafa4965
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2017-06-24 15:35:53 -0400

    Set PATH and exec-path to include MSYS2 on Windows.
    This gets package-refresh-contents working (able to run openssl) and
    git working (installed in MSYS2).

--- init.el
+++ init.el
@@ -1,3 +1,23 @@
+;; On Windows, I have a lot of things installed via MSYS2. But I don’t
+;; add tha to Windows’s PATH because that causes problems for some
+;; things. So add it explicitly to Emacs, but only if it exists.
+;; Using example from
+;; http://ergoemacs.org/emacs/emacs_env_var_paths.html
+(when (string-equal system-type "windows-nt")
+  (let
+      (
+       (paths
+	'(
+	  "C:/msys64/usr/bin"
+	  ))
+       )
+    ;; Have to also set PATH so that shell-command works at all (which
+    ;; some things use to e.g. call openssl) even though this will
+    ;; confuse any launch MS commands :-/ (e.g.,
+    ;; package-refresh-contents)
+    (setenv "PATH" (concat (getenv "PATH") (mapconcat 'identity paths ";")))
+    (setq exec-path (append exec-path paths))))
+
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
@@ -18,10 +38,7 @@
     (("gnu" . "http://elpa.gnu.org/packages/")
      ("melpa-stable" . "https://stable.melpa.org/packages/")
      ("melpa" . "https://melpa.org/packages/"))))
- '(package-selected-packages (quote (js2-mode use-package)))
- '(tls-program
-   (quote
-    ("gnutls-cli --x509cafile %t -p %p %h" "gnutls-cli --x509cafile %t -p %p %h --protocols ssl3" "openssl s_client -connect %h:%p -no_ssl2 -ign_eof" "\\msys64\\usr\\bin\\openssl s_client -connect %h:%p -no_ssl2 -ign_eof"))))
+ '(package-selected-packages (quote (js2-mode use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
