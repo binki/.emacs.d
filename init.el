6f322fa70e97827f0a8bec3050313f05365f8fa8
Nathan Phillip Brink <binki@gentoo.org>
2018-03-19 01:43:47 +0000

    Improve diff colors for PuTTY.

--- init.el
+++ init.el
@@ -44,7 +44,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(diff-added ((t (:inherit diff-changed :background "#335533" :foreground "black")))))
 
 ;; binki’s attempt to bootstrap use-package
 (unless (fboundp 'use-package)
