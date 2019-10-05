4ad165e2c9426feb1db9217c9a4a9dff039facb4
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2019-10-04 20:29:29 -0400

    Fix path appending for Windows.
    Was not adding a semicolon prior to the newly added entries.

--- init.el
+++ init.el
@@ -17,7 +17,7 @@
     ;; some things use to e.g. call openssl) even though this will
     ;; confuse any launch MS commands :-/ (e.g.,
     ;; package-refresh-contents)
-    (setenv "PATH" (concat (getenv "PATH") (mapconcat 'identity paths ";")))
+    (setenv "PATH" (concat (mapconcat 'identity (cons (getenv "PATH") paths) ";")))
     (setq exec-path (append exec-path paths))
     ;; Firefox apparently likes to lose profs.js sometimes after a Windows
     ;; BSOD/GSOD. To guard against this, manage backups of it.
